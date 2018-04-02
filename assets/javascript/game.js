9b6c5cba1ad3ac00039c1d868dbac6dcdb4d8153
Andre Myers <andre.myers99@gmail.com>
2018-04-02 03:45:17 -0400

    commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,14 +20,37 @@ $("#modal-btn").on("click", function () {
             $("#game-div").css({ "z-index": "2", "visibility": "visible" });
         }, 1500);
 
-        for (var q in response.results) {
-            setTimeout(() => {
-                
-            }, 30000);
+        var iterator = 0;
+        function questionGenerator() {
+            $("#question").html(response.results[0].question);
 
+            var ansArr = [];
+            var randomizedArr = [];
+            ansArr.push(response.results[0].incorrect_answers[0], response.results[0].incorrect_answers[1], response.results[0].incorrect_answers[2], response.results[0].correct_answer);
+            while (ansArr.length > 0) {
+                var randIndex = Math.floor(Math.random() * ansArr.length);
+                var removed = ansArr.splice(randIndex, 1);
+                randomizedArr.push(removed);
+            }
+
+            $("#answer1").html(randomizedArr[0]);
+            $("#answer2").html(randomizedArr[1]);
+            $("#answer3").html(randomizedArr[2]);
+            $("#answer4").html(randomizedArr[3]);
+
+            $(".answers").on('click', function () {
+                console.log('clicked again');
+                if ($(this).html() == response.results[0].correct_answer) {
+                    console.log('right!');
+
+                } else {
+                    console.log('wrong!');
+                }
+            })
 
         }
-
+        questionGenerator();
+        
 
     });
 
